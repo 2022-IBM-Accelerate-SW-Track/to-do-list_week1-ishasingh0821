3bb7af1f78d6473aedb0e4a72b131b2de2d65e04
ishasingh <ishasingh@vt.edu>
2022-06-12 19:07:18 -0400

    adding about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import pfp from "../assets/pfp.jpg";
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src = {pfp}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Isha Singh</div>
+            <div className="brief_description">
+            I'm a rising junior attending Virginia Tech, studying Computational Modeling and Data Analytics.
+            Outside of work, I enjoy painting, dance, fashion, and health/fitness!
+            </div>
+          </div>
+        </div>
+      </div> 
       </div>
     )
   }
